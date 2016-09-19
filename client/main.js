a7dddca0f30a6fba96ebef6ba399ff7dd55c53e3
KortoVos <larshofschlaeger@gmail.com>
2016-09-19 03:08:02 +0200

    added Name input

--- client/main.js
+++ client/main.js
@@ -1,22 +1,68 @@
+userData = new Mongo.Collection('user');
+
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+
+
+Template.designFirst.helpers({
+	user: function(){
+		//JSON.stringify(Meteor.user(),undefined,2);
+
+		//userData.insert({"test":"asdf"});
+		return data;
+	},test : function(){
+		var data = userData.find();
+		return JSON.stringify({"user":Meteor.user()},undefined,2);
+	},aboutMe: function(){
+		var data = userData.find();
+		//return JSON.stringify({"user":userData.find({ _id:Meteor.user()._id}).fetch()[0]},undefined,2);
+		return userData.find({ _id:Meteor.user()._id}).fetch()[0]
+	}
+  /*user:{
+  	firstName:"Karsten",
+  	surName:"Krachten"
+  }*/
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+
+Template.sideMenu.events({
+	'submit .new-aboutMe': function(event){
+		
+		var aboutMeText = event.target.aboutMe.value;
+		userData.update(Meteor.user()._id,
+			{
+				$set:{
+					_id:Meteor.user()._id,
+					"cvData.aboutMe":aboutMeText
+				}
+			},{
+				upsert:true
+			}
+			);
+			return false;
+	},'submit .new-userName': function(event){
+		var firstname = event.target.firstName.value;
+		var surName = event.target.surName.value;
+		userData.update(Meteor.user()._id,
+			{
+				$set:{
+					"cvData.firstName":firstname,
+					"cvData.surName":surName
+				}
+			},{
+				upsert:true
+			}
+			);
+			return false;
+	}
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+
+
+Accounts.ui.config({
+	passwordSignupFields:"USERNAME_AND_OPTIONAL_EMAIL"
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
+userData = new Mongo.Collection('user');
+
 import { Meteor } from 'meteor/meteor';
 
+
 Meteor.startup(() => {
-  // code to run on server at startup
+  
 });
